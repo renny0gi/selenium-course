107ccdcc10ac5f8acd2951334159da729459b2e8
rgimadin <rinat.gimadinov@t-systems.com>
2019-11-10 23:15:50 +0300

    added LoginPage and its locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,23 @@
 import time
 
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/"
+
+link = 'http://selenium1py.pythonanywhere.com/'
+# link = ' http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link).open()
     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
     # time.sleep(5)
+
+def test_check_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link).open()
+    page.should_be_login_page()
