8c44c4e720c663c8bdbc604b1d6e15381bfe7615
rgimadin <rinat.gimadinov@t-systems.com>
2019-11-11 17:46:08 +0300

    created listOfUrls from generator + added xfail

--- test_product_page.py
+++ test_product_page.py
@@ -3,26 +3,19 @@ import pytest
 
 from pages.product_page import ProductPage
 
-# url_product_promo = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-url_product_promo = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7'
+BASIC_PROMO_URL = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
 
-list_of_urls = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", \
-        marks=pytest.mark.xfail),
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+list_of_urls = [f'{BASIC_PROMO_URL}/?promo=offer{num}' for num in range(10)]
+# Mark 8-th url as xfail since we know it's not gonna be fixed any time soon
+list_of_urls[7] = pytest.param(list_of_urls[7], marks=pytest.mark.xfail)
 
 def test_guest_can_see_product_info(browser):
+    url_product_promo = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     product_page = ProductPage(browser, url_product_promo).open()
     product_page.should_be_product_name()
     product_page.should_be_product_price()
     product_page.should_be_product_description
+
 @pytest.mark.parametrize('url_product_promo', list_of_urls)
 def test_guest_can_add_product_to_basket(browser, url_product_promo):
     product_page = ProductPage(browser, url_product_promo).open()
@@ -35,7 +28,6 @@ def test_guest_can_add_product_to_basket(browser, url_product_promo):
     product_page = product_page.add_to_basket()
     
     product_page.solve_quiz_and_get_code()
-    time.sleep(5)
     
     product_page.check_add_msg_with_product_name(name)
     product_page.check_add_msg_with_basket_sum(price)
