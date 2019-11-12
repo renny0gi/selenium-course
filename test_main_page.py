bdd1f89d6ab29c8b36e05d0ec9e6ca6a9feaf2f7
rgimadin <rinat.gimadinov@t-systems.com>
2019-11-12 14:58:26 +0300

    login_tests were aggregated to TestLoginFromMainPage with mark login_guest

--- test_main_page.py
+++ test_main_page.py
@@ -8,15 +8,17 @@ from pages.basket_page import BasketPage
 
 link = 'http://selenium1py.pythonanywhere.com/'
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link).open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link).open()
+        page.should_be_login_link()
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link).open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link).open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 @pytest.mark.check_basket
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
@@ -27,9 +29,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_empty= 'Your basket is empty. Continue shopping'
     basket_page.should_see_text_empty_basket()
     basket_page.check_text_empty_basket(basket_empty)
-
-
-# def test_check_login_page(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
-#     page = LoginPage(browser, link).open()
-#     page.should_be_login_page()
